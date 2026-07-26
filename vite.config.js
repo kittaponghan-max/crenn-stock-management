6c1b4726d5504f6dde9e89e1a5bbf190f9034c66
kittaponghan-max <kittapong.han@gmail.com>
2026-07-26 20:40:49 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  base: './', // เพิ่มบรรทัดนี้เพื่อให้แอปหาไฟล์ CSS/JS เจอไม่ว่าจะอยู่ที่ URL ไหน
   build: {
     outDir: 'dist',
   }
