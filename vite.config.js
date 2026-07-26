28cf988342ce093a94da1f4a0dd681fb7784c629
kittaponghan-max <kittapong.han@gmail.com>
2026-07-26 21:43:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
-})
+});
